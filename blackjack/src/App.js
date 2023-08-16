1d77e35d270619259a37aafbb9f8017a3402b469
JCline11 <jasonsmevog@gmail.com>
2023-08-15 23:56:41 -0700

    Created deck

--- blackjack/src/App.js
+++ blackjack/src/App.js
@@ -3,29 +3,69 @@ import './App.css';
 import {useState} from "react"
 
 function App() {
-  const [number, setNumber] = useState(0) 
-  function buttonInc() {
-    setNumber(number + 1)
+  let card = [["Ace", 1, 4], ["Two", 2, 4], ["Three", 3, 4], ["Four", 4, 4], ["Five", 5, 4], ["Six", 6, 4], ["Seven", 7, 4], ["Eight", 8, 4], ["Nine", 9, 4], ["Ten", 10, 4], ["Jack", 10, 4], ["Queen", 10, 4], ["King", 10, 4]];
+  let deck = [];
+    while(card.length > 0){ // get a random value, reduce that value from array, check if reduced to 0 then remove it.
+      let max = 12;
+      var rand = getRandomInt(0, max);
+      var num = card[rand];
+      var suit;
+
+      if (num && num[2] > 0)
+      {
+        switch (num[2])
+        {
+          case 4: suit = `${num[0]} of Hearts`;
+            break;
+          case 3: suit = `${num[0]} of Spades`;
+            break;
+          case 2: suit = `${num[0]} of Diamonds`;
+            break;  
+          case 1: suit = `${num[0]} of Clubs`;
+            break;
+        }
+        deck.push([suit, num[1]]);
+        num[2]--;
+      }
+
+      else if (num)
+      {
+        card = card.filter(item => item !== num);
+        console.log(card);
+        max--;
+      }
+    }
+    console.log(deck);
+
+  const [number, setRandomInt] = useState(0)
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  
   return (
+    
     <div className="App">
       <header className="App-header">
-        {/* <img src="https://media.tenor.com/2gjvgYyOusYAAAAS/rocket-league-cosplay.gif" className="App-logo" alt="logo" /> */}
         <p>
-          <button type="button" onClick={()=>buttonInc()}>Click Me!</button> 
+          <button type="button" onClick={()=>getRandomInt(1, 52)}>Hit!</button> 
+          {number}
+          <button type="button" onClick={()=>getRandomInt(1, 52)}>Stand.</button> 
           {number}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+/* 
+1. make balckjack
+2. make hit and stand buttons that work
+3. deal 2 random cards to dealer and player
+4. make 1 card of dealer face down
+5. make it so if a A is dealt can choose between 11 and 1
+6. make it so if dealer is less than 17, has to hit
+7. make it so if hit 21 confetti rains down 
+
+*/
